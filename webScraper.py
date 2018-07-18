055dbf42d2d8599846a382f0455d50540012d072
sarahjtracy <sarahtracy14@gmail.com>
2018-07-17 23:56:41 -0700

    Delete webScraper.py

--- webScraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-scrapeUrl = "http://www.eloratings.net/2018_World_Cup"
-scrapePage = requests.get(scrapeUrl)
-
-soup = BeautifulSoup(scrapePage.content, 'html.parser')
-
-print scrapePage.content
